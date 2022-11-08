51ace884d2ceacd119346e742cdb7d447d8e0d06
pRaNaY <www.pranay1337.com@gmail.com>
2022-11-08 18:01:31 +0530

    :sparkles: Created navigation related classes for login/signup module

---
+++ app/src/main/java/com/pranay/jetkite/navigation/JetKiteNavHost.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pranay.jetkite.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.pranay.jetkite.login.navigation.HomeDestination
+import com.pranay.jetkite.login.navigation.LoginDestination
+import com.pranay.jetkite.login.navigation.SplashDestination
+import com.pranay.jetkite.login.navigation.WelcomeDestination
+import com.pranay.jetkite.login.navigation.homeScreen
+import com.pranay.jetkite.login.navigation.loginScreen
+import com.pranay.jetkite.login.navigation.splashScreen
+import com.pranay.jetkite.login.navigation.welcomeScreen
+
+/**
+ * Top-level navigation graph. Navigation is organized as explained at
+ * https://d.android.com/jetpack/compose/nav-adaptive
+ *
+ * The navigation graph defined in this file defines the different top level routes. Navigation
+ * within each route is handled using state and Back Handlers.
+ */
+@Composable
+fun JetKiteNavHost(
+    navController: NavHostController,
+    onNavigateToDestination: (JetKiteNavDestination, String) -> Unit,
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    startDestination: String = SplashDestination.route,
+    isExpandedScreen: Boolean
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        splashScreen {
+            onNavigateToDestination(WelcomeDestination, WelcomeDestination.route)
+        }
+        welcomeScreen(onActionLoginClick = {
+            onNavigateToDestination(LoginDestination, LoginDestination.route)
+        }, onActionSignUpClick = {}, onNavigationBackClick = {})
+        loginScreen(
+            onLoginSuccess = {
+                onNavigateToDestination(HomeDestination, HomeDestination.route)
+            },
+            onNavigationBackClick = {},
+            onForgotClick = {
+            },
+            onSwitchAccountClick = {
+            }
+        )
+        homeScreen {
+            onNavigateToDestination(WelcomeDestination, WelcomeDestination.route)
+        }
+    }
+}
---
+++ feature/login/src/main/java/com/pranay/jetkite/login/SplashScreen.kt
@@ -0,0 +1,79 @@
+package com.pranay.jetkite.login
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProgressIndicatorDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.pranay.jetkite.components.JetKiteLogo
+import com.pranay.jetkite.components.extension.LightDarkPreview
+import com.pranay.jetkite.designsystem.JetKiteTheme
+import com.pranay.jetkite.designsystem.spacing
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(
+    onNavigationBackClick: () -> Unit = {},
+    onLoadingCompleted: () -> Unit
+) {
+    /*val progressValue = 1f
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val progressAnimationValue by infiniteTransition.animateFloat(
+        initialValue = 0.0f,
+        targetValue = progressValue,
+        animationSpec = infiniteRepeatable(animation = tween(5000))
+    )*/
+
+    var progress by remember { mutableStateOf(0.1f) }
+    val animatedProgress = animateFloatAsState(
+        targetValue = progress,
+        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec
+    ).value
+
+    Surface {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize().padding(horizontal = MaterialTheme.spacing.medium),
+            verticalArrangement = Arrangement.Center
+        ) {
+            JetKiteLogo(
+                modifier = Modifier
+                    .size(height = MaterialTheme.spacing.largeImage, width = MaterialTheme.spacing.largeImage)
+                    .padding(vertical = MaterialTheme.spacing.medium)
+            )
+            Spacer(modifier = Modifier.padding(MaterialTheme.spacing.large))
+            LinearProgressIndicator(progress = animatedProgress)
+            LaunchedEffect(progress) {
+                if (progress <= 1f) {
+                    delay(500)
+                    progress += 0.1f
+                } else {
+                    onLoadingCompleted()
+                }
+            }
+        }
+    }
+}
+
+@LightDarkPreview
+@Composable
+fun LoadingScreenPreview() {
+    JetKiteTheme {
+        SplashScreen {}
+    }
+}
