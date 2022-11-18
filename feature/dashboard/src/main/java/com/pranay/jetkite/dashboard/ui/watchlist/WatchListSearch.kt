bc2e7df2e6a00b74b2a6c19a4186c0d2de308a5d
pRaNaY <www.pranay1337.com@gmail.com>
2022-11-18 13:32:27 +0530

    :art: Updated searchbox design and added in watch screen

--- feature/dashboard/src/main/java/com/pranay/jetkite/dashboard/ui/watchlist/WatchListSearch.kt
+++ feature/dashboard/src/main/java/com/pranay/jetkite/dashboard/ui/watchlist/WatchListSearch.kt
@@ -1,6 +1,5 @@
 package com.pranay.jetkite.dashboard.ui.watchlist
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -18,12 +17,14 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -33,7 +34,6 @@ import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.pranay.jetkite.components.JetKiteTextView
 import com.pranay.jetkite.components.extension.LightDarkPreviews
-import com.pranay.jetkite.components.extension.clickableWithRipple
 import com.pranay.jetkite.components.icons.JetKiteIcons
 import com.pranay.jetkite.dashboard.R
 import com.pranay.jetkite.designsystem.JetKiteTheme
@@ -59,56 +59,64 @@ fun WatchListSearch(
     textColor: Color = if (isSystemInDarkTheme()) colorSearchTextDark else colorSearchTextLight,
     onValueChange: (TextFieldValue) -> Unit
 ) {
-    val searchValue by remember { mutableStateOf(TextFieldValue("")) }
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .height(TextFieldDefaults.MinHeight)
-            .background(backgroundColor, RoundedCornerShape(MaterialTheme.spacing.extraSmall)),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.small)
+    var searchValue by remember { mutableStateOf(TextFieldValue("")) }
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(MaterialTheme.spacing.extraSmall),
+        color = backgroundColor,
+        shadowElevation = MaterialTheme.spacing.extraSmall
     ) {
-        OutlinedTextField(
-            value = searchValue,
-            onValueChange = onValueChange,
-            modifier = Modifier.weight(1f),
-            leadingIcon = {
-                androidx.compose.material.Icon(
-                    imageVector = JetKiteIcons.Search,
-                    contentDescription = stringResource(id = R.string.str_search),
-                    tint = textColor,
-                    modifier = Modifier.clickableWithRipple {
-                        // Do with search click
-                    }
-                )
-            },
-            colors = TextFieldDefaults.textFieldColors(
-                textColor = textColor,
-                cursorColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                unfocusedIndicatorColor = Color.Transparent,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedLabelColor = MaterialTheme.colorScheme.outline,
-                focusedLabelColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                containerColor = Color.Transparent,
-                unfocusedTrailingIconColor = MaterialTheme.colorScheme.outline,
-                focusedTrailingIconColor = MaterialTheme.colorScheme.onPrimaryContainer
-            )
-        )
-        totalWatchListItem?.let {
-            JetKiteTextView(text = it)
-        }
-        Divider(
+        Row(
             modifier = Modifier
-                .fillMaxHeight()
-                .padding(vertical = MaterialTheme.spacing.dp12)
-                .width(MaterialTheme.spacing.dp1)
-        )
-        IconButton(onClick = onFilterClick) {
-            Icon(
-                imageVector = Icons.Default.List,
-                contentDescription = stringResource(id = R.string.str_search),
-                tint = textColor
+                .fillMaxWidth()
+                .height(TextFieldDefaults.MinHeight),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.small)
+        ) {
+            OutlinedTextField(
+                value = searchValue,
+                onValueChange = {
+                    searchValue = it
+                    onValueChange.invoke(it)
+                },
+                modifier = Modifier.weight(1f),
+                leadingIcon = {
+                    IconButton(onClick = { }) {
+                        Icon(
+                            imageVector = JetKiteIcons.Search,
+                            contentDescription = stringResource(id = R.string.str_search),
+                            tint = textColor
+                        )
+                    }
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor = textColor,
+                    cursorColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedLabelColor = MaterialTheme.colorScheme.outline,
+                    focusedLabelColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    containerColor = Color.Transparent,
+                    unfocusedTrailingIconColor = MaterialTheme.colorScheme.outline,
+                    focusedTrailingIconColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
             )
+            totalWatchListItem?.let {
+                JetKiteTextView(text = it)
+            }
+            Divider(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(vertical = MaterialTheme.spacing.dp12)
+                    .width(MaterialTheme.spacing.dp1)
+            )
+            IconButton(onClick = onFilterClick) {
+                Icon(
+                    imageVector = Icons.Default.List,
+                    contentDescription = stringResource(id = R.string.str_search),
+                    tint = textColor
+                )
+            }
         }
     }
 }
