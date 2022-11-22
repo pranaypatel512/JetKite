2f3cde1de32661e96ffd9babc15a95528861a16c
pRaNaY <www.pranay1337.com@gmail.com>
2022-11-22 16:08:57 +0530

    :art: Created tools screen related tabs and composable

---
+++ feature/dashboard/src/main/java/com/pranay/jetkite/dashboard/ui/tools/ToolsScreenViewModel.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pranay.jetkite.dashboard.ui.tools
+
+import androidx.lifecycle.ViewModel
+import com.pranay.jetkite.dashboard.R
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class ToolsScreenViewModel @Inject constructor() : ViewModel() {
+
+    private val _tabState = MutableStateFlow(
+        ToolsScreenTabState(
+            titles = listOf(R.string.str_basket, R.string.str_sip, R.string.str_alerts),
+            currentIndex = 0
+        )
+    )
+    val tabState: StateFlow<ToolsScreenTabState> = _tabState.asStateFlow()
+
+    fun switchTab(newIndex: Int) {
+        if (newIndex != tabState.value.currentIndex) {
+            _tabState.update {
+                it.copy(currentIndex = newIndex)
+            }
+        }
+    }
+}
+
+data class ToolsScreenTabState(
+    val titles: List<Int>,
+    val currentIndex: Int
+)
